187445ae27872fabef502d45404e2c33457f191c
Russel-DH <44677975+Russel-DH@users.noreply.github.com>
2024-05-21 21:14:27 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['banana', 'mango', 'qiwi', 'orange', 'leechie', 'dragon fruit']
+print(my_list)
+print(my_list[0]+',', my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'somefruit'
+print(my_list)
+
+my_dict = {'a' : 'AaAa', 'b' : 'BbBb', 'c' : 'CcCc', 'd' : 'DdDd', 'e' : 'EeEe', 'f' : 'FfFf'}
+print(my_dict)
+print(my_dict['b'])
+my_dict['c'] = 'Cc__'
+my_dict['g'] = 'GgGg'
+print(my_dict)
